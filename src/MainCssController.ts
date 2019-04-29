a96ae6970011a451b017462de680eb2a30d6344f
Hide-KC <kcs.dev.labo@gmail.com>
2019-04-29 23:53:35 +0900

    Sushi表示機能完成、再起動で適用される

--- src/MainCssController.ts
+++ src/MainCssController.ts
@@ -1,16 +1,28 @@
 import * as fs from 'fs';
 import * as cp from 'child_process';
+import * as vscode from 'vscode';
+import * as path from 'path';
 
 export default class MainCssController {
+  private static instance: MainCssController;
   private extName: string;
   private mainCssPath: string;
+  private backupName: string;
 
-  constructor(
+  private constructor(
     extName: string,
     mainCssPath: string
   ) {
     this.extName = extName;
     this.mainCssPath = mainCssPath;
+    this.backupName = this.mainCssPath + '.backup';
+  }
+
+  static getInstance(extName: string, mainCssPath: string){
+    if (!this.instance){
+      this.instance = new MainCssController(extName, mainCssPath);
+    }
+    return this.instance;
   }
 
   //workbench.main.cssのコンテンツを取得するとかなり長い文字列
@@ -22,29 +34,63 @@ export default class MainCssController {
    * if there are no backup files.
    */
   generateBackup() {
-    const backupName = this.mainCssPath + '.backup';
-    if (fs.existsSync(backupName)) {
-      fs.copyFileSync(this.mainCssPath, backupName, fs.constants.COPYFILE_EXCL);
+    if (!fs.existsSync(this.backupName)) {
+      fs.copyFileSync(this.mainCssPath, this.backupName, fs.constants.COPYFILE_EXCL);
     }
   }
+  
+  /**
+   * Generate the Css Content.
+   * @param order sushi neta
+   */
+  generateContent(order: string): string {
+    //resフォルダを強引に取得
+    const resPath = path.join(__dirname.substring(0, __dirname.length - 4), 'res');
+    
+    //pngを強引に取得
+    const orderPath = path.join(resPath, `sushi_${order}.png`).replace(/\\/g, '\\\\\\\\\\\\\\\\');
 
-  getCssContent(): string {
-    return "";
+    if (fs.existsSync(orderPath)) {
+      console.log("Exists!");
+    } else {
+      //ネタ名が間違えてたらスロー
+      throw new ReferenceError();
+    }
+    
+    const prefs = vscode.workspace.getConfiguration('Sushi Buffet Preferences');
+    const opacity = prefs.get<number|undefined>('opacity') ? prefs.get<number|undefined>('opacity') : 0.8;
+    
+    return `/\\*ext-${this.extName}-start\\*/body\\{background-position:center center;background-repeat:no-repeat;background-attachment:fixed;background-size:250px auto;opacity:${opacity};background-image:url\\("${orderPath}"\\);\\}/\\*ext-${this.extName}-end\\*/`;
+  }
+
+  addCssContent(order: string) {
+    const content = this.generateContent(order);
+    const delContent = `/\\*ext-${this.extName}-start\\*/.*/\\*ext-${this.extName}-end\\*/`;
+    console.log(content);
+
+    cp.exec(`sed -i -e "s|${delContent}||g" -e "$a ${content}" -e "/^[<space><tab>]*$/d" "${this.mainCssPath}"`, (err, stdout, stderr) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(stdout);
+      }
+    });
   }
   
-  addCssContent(): string {
-    return "";
-  }
-  
+  /**
+   * Reset content of workbench.main.css to original.
+   */
   resetCssContent() {
-    const exec = cp.exec;
-
-
-
-
-    let content = this.getCssContent();
-    const reg = new RegExp(`\\/\\*ext-${this.extName}-start\\*\\/[\\s\\S]*?\\/\\*ext-${this.extName}-end\\*\\/`,'g');
-    content = content.replace(reg, '');
-    content = content.replace(/\s*$/, '');
+    //削除する文字列を正規表現で指定
+    const delContent = `/\\*ext-${this.extName}-start\\*/.*/\\*ext-${this.extName}-end\\*/`;
+    
+    //sed で対象文字列を削除→空行を削除→最終行の改行コードを削除
+    cp.exec(`sed -i -e "s|${delContent}||g" -e "/^[<space><tab>]*$/d" -e "/\\n$/d" -e"/\\r$/d" "${this.mainCssPath}"`, (err, stdout, stderr) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(stdout);
+      }
+    });
   }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,54 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
+import MainCssController from './MainCssController';
 
 export function activate(context: vscode.ExtensionContext) {
+  if (require.main){
+    const base = path.dirname(require.main.filename);
+    const filePath = path.join(base, 'vs', 'workbench', 'workbench.main.css');
 
+    console.log(base);
+    console.log(filePath);
+
+    const controller = MainCssController.getInstance('sushi-buffet', filePath);
+    controller.addCssContent('akami');
+  }
+  
+  //at orderd Sushi
+  const order = vscode.commands.registerCommand("extension.order", () => {
+    //Inflate QuickPick
+  });
+  context.subscriptions.push(order);
+
+  //reset cover
+  const reset = vscode.commands.registerCommand("extension.reset", () => {
+    if (require.main){
+      const base = path.dirname(require.main.filename);
+      const filePath = path.join(base, 'vs', 'workbench', 'workbench.main.css');  
+      const controller = MainCssController.getInstance('sushi-buffet', filePath);
+      controller.resetCssContent();
+    }
+  })
+  context.subscriptions.push(reset);
+
+  const textChanged = vscode.workspace.onDidChangeTextDocument((e) => {
+    //e.contentChanges
+    const last = e.contentChanges.length;
+    if(last > 0){
+      console.log(e.contentChanges[last-1]);
+      console.log("* * *");
+    }
+  });
+  context.subscriptions.push(textChanged);
 }
 
-export function deactivate() {}
+export function deactivate() {
+  console.log('deactivate');
+  if (require.main){
+    const base = path.dirname(require.main.filename);
+    const filePath = path.join(base, 'vs', 'workbench', 'workbench.main.css');
+
+    const controller = MainCssController.getInstance('sushi-buffet', filePath);
+    controller.resetCssContent();
+  }
+}
